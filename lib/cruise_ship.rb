3432b6c7fb6f6ee9238ad7af26f617730c0849c6
Jung Lee <lee.jung.s95@gmail.com>
2020-04-28 22:09:54 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -11,6 +11,5 @@ def select_winner(passengers)
     if suite == :suite_a && name.start_with?("A")
       winner = name
     end
-  end
-  winner# add the code snippet here!
+  end# add the code snippet here!
 end
