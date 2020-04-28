be1e1900f7b0caed2e80ecdd85ffeaf6c212bb96
Jung Lee <lee.jung.s95@gmail.com>
2020-04-28 22:05:08 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,13 +7,6 @@
 # }
 
 def select_winner(passengers)
-  passengers = {
-  suite_a: "Amanda Presley",
-  suite_b: "Seymour Hoffman",
-  suite_c: "Alfred Tennyson",
-  suite_d: "Charlie Chaplin",
-  suite_e: "Crumpet the Elf"
-  }
   return passengers[suite_a]
   # add the code snippet here!
 end
